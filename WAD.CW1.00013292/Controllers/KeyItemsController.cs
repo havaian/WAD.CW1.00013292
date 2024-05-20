d9299677f83254536aafb3b915dd277714fb2419
havaian <m.usman.work@gmail.com>
2024-05-20 14:54:14 +0500

    Commit #2 (uploading the proj.)

---
+++ WAD.CW1.00013292/Controllers/KeyItemsController.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD.CW1._00013292.DAL.Dtos;
+using WAD.CW1._00013292.DAL.Interfaces;
+using WAD.CW1._00013292.Models;
+
+namespace WAD.CW1._00013292.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KeyItemsController : ControllerBase
+    {
+        private readonly IKeyItemRepository _keyItemRepository;
+        private readonly IMapper _mapper;
+
+        public KeyItemsController(IKeyItemRepository keyItemRepository, IMapper mapper)
+        {
+            _keyItemRepository = keyItemRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllKeyItems()
+        {
+            var keyItems = await _keyItemRepository.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<KeyItemDto>>(keyItems));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetKeyItem(int id)
+        {
+            var keyItem = await _keyItemRepository.GetByIdAsync(id);
+            if (keyItem == null)
+            {
+                return NotFound($"KeyItem with ID {id} not found");
+            }
+            return Ok(_mapper.Map<KeyItemDto>(keyItem));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateKeyItem(KeyItemDto keyItemDto)
+        {
+            var keyItem = _mapper.Map<KeyItem>(keyItemDto);
+            var createdKeyItem = await _keyItemRepository.AddAsync(keyItem);
+            return Ok($"Created KeyItem with ID {createdKeyItem.KeyItemId}");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateKeyItem(int id, KeyItemDto keyItemDto)
+        {
+            if (id != keyItemDto.KeyItemId)
+            {
+                return BadRequest("ID mismatch");
+            }
+
+            var keyItemToUpdate = await _keyItemRepository.GetByIdAsync(id);
+            if (keyItemToUpdate == null)
+            {
+                return NotFound($"KeyItem with ID {id} not found");
+            }
+
+            _mapper.Map(keyItemDto, keyItemToUpdate);
+            await _keyItemRepository.UpdateAsync(keyItemToUpdate);
+
+            return Ok($"Updated KeyItem with ID {id}");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteKeyItem(int id)
+        {
+            var keyItem = await _keyItemRepository.GetByIdAsync(id);
+            if (keyItem == null)
+            {
+                return NotFound($"KeyItem with ID {id} not found");
+            }
+
+            await _keyItemRepository.DeleteAsync(id);
+            return Ok($"Deleted KeyItem with ID {id}");
+        }
+    }
+}
---
+++ WAD.CW1.00013292/Controllers/KeyTypesController.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD.CW1._00013292.DAL.Dtos;
+using WAD.CW1._00013292.DAL.Interfaces;
+using WAD.CW1._00013292.Models;
+
+namespace WAD.CW1._00013292.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KeyTypesController : ControllerBase
+    {
+        private readonly IKeyTypeRepository _keyTypeRepository;
+        private readonly IMapper _mapper;
+
+        public KeyTypesController(IKeyTypeRepository keyTypeRepository, IMapper mapper)
+        {
+            _keyTypeRepository = keyTypeRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllKeyTypes()
+        {
+            var keyTypes = await _keyTypeRepository.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<KeyTypeDto>>(keyTypes));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetKeyType(int id)
+        {
+            var keyType = await _keyTypeRepository.GetByIdAsync(id);
+            if (keyType == null)
+            {
+                return NotFound($"KeyType with ID {id} not found");
+            }
+            return Ok(_mapper.Map<KeyTypeDto>(keyType));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateKeyType(KeyTypeDto keyTypeDto)
+        {
+            var keyType = _mapper.Map<KeyType>(keyTypeDto);
+            var createdKeyType = await _keyTypeRepository.AddAsync(keyType);
+            return Ok($"Created KeyType with ID {createdKeyType.KeyTypeId}");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateKeyType(int id, KeyTypeDto keyTypeDto)
+        {
+            if (id != keyTypeDto.KeyTypeId)
+            {
+                return BadRequest("ID mismatch");
+            }
+
+            var keyTypeToUpdate = await _keyTypeRepository.GetByIdAsync(id);
+            if (keyTypeToUpdate == null)
+            {
+                return NotFound($"KeyType with ID {id} not found");
+            }
+
+            _mapper.Map(keyTypeDto, keyTypeToUpdate);
+            await _keyTypeRepository.UpdateAsync(keyTypeToUpdate);
+
+            return Ok($"Updated KeyType with ID {id}");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteKeyType(int id)
+        {
+            var keyType = await _keyTypeRepository.GetByIdAsync(id);
+            if (keyType == null)
+            {
+                return NotFound($"KeyType with ID {id} not found");
+            }
+
+            await _keyTypeRepository.DeleteAsync(id);
+            return Ok($"Deleted KeyType with ID {id}");
+        }
+    }
+}
